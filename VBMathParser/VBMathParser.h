20ba7e1153c2013c7c7423ff3d39643d1754f1e8
Valeriy Bezuglyy <valnocorner@gmail.com>
2014-07-10 22:49:54 +0400

    description

--- VBMathParser/VBMathParser.h
+++ VBMathParser/VBMathParser.h
@@ -22,6 +22,77 @@
 //    SOFTWARE.
 //
 
+//    VBMathParser
+//    ============
+//
+//    VBMathParser is a simple framework to perform mathematical expressions parsing.
+//    Math parser is conceived as a one-line string-to-double converter.
+//
+//    This framework uses ARC. DO NOT FORGET to turn on ARC for framework's sources (-fobjc-arc flag), if your project is developed in non-ARC environment.
+//
+//    ## How to use
+//    1. Drag VBMathParser dir into your project.
+//    2. Import header
+//
+//    `#import "VBMathParser.h”`
+//
+//    3. Use one of the variants
+//
+//    Single line
+//
+//    double result = [VBMathParser evaluateExpression:@"1"];
+//
+//    Using instance method
+//
+//    VBMathParser* parser = [[VBMathParser alloc] initWithExpression:@"2 + 4"];
+//    result = [parser evaluate];
+//
+//    Expression can always be changed by setting the expression property of VBMathParser object
+//
+//    parser.expression = @"2(1+3)";
+//    result = [parser evaluate];
+//
+//    To use variables declare them before assigning new expression (single line variant included)
+//
+//    parser.vars = @[@"x"];
+//    parser.expression = @"x+1";
+//    result = [parser evaluateWithVarsValues:@{@"x": @(2)}];
+//
+//    ## Expected syntax
+//    1. If you open a bracket - do not forget to close it later.
+//    2. All operations are expected to be used in mathematical expressions as binary operations.
+//
+//    Only "-" operation can be used both as binary and as unary one. Actually unary minus operation is always replaced by a binary one as the following:
+//
+//    "-4" -> "0-4"
+//    "2 * (-4)" -> "2 * (0 - 4)"
+//    "-abs(4)" -> "0 - abs(4)"
+//
+//    3. All functions are expected to be followed by an argument enclosed in brackets.
+//
+//    abs3 - error!
+//    abs(3) - OK
+//
+//    4. Variable name must consist of at least one letter plus letters and numbers.
+//
+//    valid names: x, y, x1, x01, etc
+//    invalid names: 0x, x_213, etc
+//
+//    ## Supported features
+//    1. brackets: (, )
+//    2. operations: +, - (unary/binary), *, /, ^(power)
+//    3. functions: abs, sin, cos, tan
+//    4. variables
+//    5. constants: pi
+//
+//    ## Coming soon
+//    1. more functions and constants
+//
+//    Feel free to left a feature request
+//
+//    ## License
+//    VBMathParser is available under the MIT license. See the LICENSE file for more info.
+
 #import <Foundation/Foundation.h>
 
 @interface VBMathParser : NSObject
