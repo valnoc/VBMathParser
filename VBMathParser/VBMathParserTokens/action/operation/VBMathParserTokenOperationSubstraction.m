2dd288800dfb957372a0676c74edccc4543712d1
Valeriy Bezuglyy <valnocorner@gmail.com>
2015-05-24 12:55:54 +0300

    license update

--- VBMathParser/VBMathParserTokens/action/operation/VBMathParserTokenOperationSubstraction.m
+++ VBMathParser/VBMathParserTokens/action/operation/VBMathParserTokenOperationSubstraction.m
@@ -1,7 +1,7 @@
 //
 //    The MIT License (MIT)
 //
-//    Copyright (c) 2014 Valeriy Bezuglyy. All rights reserved.
+//    Copyright (c) 2015 Valeriy Bezuglyy.
 //
 //    Permission is hereby granted, free of charge, to any person obtaining a copy
 //    of this software and associated documentation files (the "Software"), to deal
