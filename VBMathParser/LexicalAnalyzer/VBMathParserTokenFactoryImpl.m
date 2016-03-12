0a4282b8c434ed8b09e1055f5b81d70b286dd438
Valeriy Bezuglyy <valnocorner@gmail.com>
2016-03-12 16:27:46 +0300

    token creation

--- VBMathParser/LexicalAnalyzer/VBMathParserTokenFactoryImpl.m
+++ VBMathParser/LexicalAnalyzer/VBMathParserTokenFactoryImpl.m
@@ -8,12 +8,61 @@
 
 #import "VBMathParserTokenFactoryImpl.h"
 
+#import "VBMathParserTokenSpecialBracketOpen.h"
+#import "VBMathParserTokenSpecialBracketClose.h"
+
+#import "VBMathParserTokenNumber.h"
+#import "VBMathParserTokenVar.h"
+
+#import "VBMathParserTokenConstPi.h"
+
+#import "VBMathParserTokenOperationAddition.h"
+#import "VBMathParserTokenOperationDivision.h"
+#import "VBMathParserTokenOperationMultiplication.h"
+#import "VBMathParserTokenOperationPower.h"
+#import "VBMathParserTokenOperationSubstraction.h"
+
+#import "VBMathParserTokenFunctionAbs.h"
+#import "VBMathParserTokenFunctionCos.h"
+#import "VBMathParserTokenFunctionSin.h"
+#import "VBMathParserTokenFunctionTan.h"
+
 @implementation VBMathParserTokenFactoryImpl
 
 - (VBMathParserToken*) tokenWithType:(NSString*) tokenType
                               string:(NSString*) string {
-#warning TODO
-    return nil;
+    
+    NSArray* allClasses = @[[VBMathParserTokenSpecialBracketOpen class],
+                            [VBMathParserTokenSpecialBracketClose class],
+                           
+                            [VBMathParserTokenNumber class],
+                            [VBMathParserTokenVar class],
+                           
+                            [VBMathParserTokenConstPi class],
+                           
+                            [VBMathParserTokenOperationAddition class],
+                            [VBMathParserTokenOperationDivision class],
+                            [VBMathParserTokenOperationMultiplication class],
+                            [VBMathParserTokenOperationPower class],
+                            [VBMathParserTokenOperationSubstraction class],
+                           
+                            [VBMathParserTokenFunctionAbs class],
+                            [VBMathParserTokenFunctionCos class],
+                            [VBMathParserTokenFunctionSin class],
+                            [VBMathParserTokenFunctionTan class]
+                            ];
+    allClasses = [allClasses filteredArrayUsingPredicate:
+                  [NSPredicate predicateWithFormat:@"tokenType like %@", tokenType]];
+    
+    VBMathParserToken* token = nil;
+    for (Class tokenClass in allClasses) {
+        if ([tokenClass isToken:string]) {
+            token = [tokenClass tokenWithString:string];
+            break;
+        }
+    }
+    
+    return token;
 }
 
 @end
