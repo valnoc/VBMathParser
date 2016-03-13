6d5ab1d53565de3ae39e6bbe73b6015b0276afa7
Valeriy Bezuglyy <valnocorner@gmail.com>
2016-03-13 16:35:08 +0300

    rpn evaluation

--- VBMathParser/ReversePolishNotationWorker/VBMathParserDefaultRPNCalculator.m
+++ VBMathParser/ReversePolishNotationWorker/VBMathParserDefaultRPNCalculator.m
@@ -79,6 +79,88 @@
     return tokens;
 }
 
+- (double) evaluateExpression:(NSArray<VBMathParserToken *> *)expression
+               withVarsValues:(NSDictionary<NSString *,NSNumber *> *)varsValues {
+    NSMutableArray* resultArray = [NSMutableArray arrayWithArray:expression];
+    
+    [self replaceVarTokens:resultArray
+                withValues:varsValues];
+    [self replaceConstTokens:resultArray];
+    
+    //  evaluate
+    for (NSInteger i = 0; i < resultArray.count; i++) {
+        VBMathParserToken* token = resultArray[i];
+        
+        if ([token isKindOfClass:[VBMathParserTokenAction class]]) {
+            NSRange replacementRange = NSMakeRange(i, 1);
+            double paramRight = 0;
+            double paramLeft = 0;
+            
+            if (i > 0) {
+                VBMathParserToken* tokenRight = resultArray[i - 1];
+                if ([tokenRight isKindOfClass:[VBMathParserTokenNumber class]] || [tokenRight isKindOfClass:[NSNumber class]]) {
+                    paramRight = [(VBMathParserTokenNumber*)tokenRight doubleValue];
+                }
+                
+                replacementRange.location--;
+                replacementRange.length++;
+            }
+            
+            if ([token isKindOfClass:[VBMathParserTokenOperation class]] && i > 1) {
+                VBMathParserToken* tokenLeft = resultArray[i - 2];
+                if ([tokenLeft isKindOfClass:[VBMathParserTokenNumber class]] || [tokenLeft isKindOfClass:[NSNumber class]]) {
+                    paramLeft = [(VBMathParserTokenNumber*)tokenLeft doubleValue];
+                }
+                replacementRange.location--;
+                replacementRange.length++;
+            }
+            
+            double actRes = 0;
+            if ([token isKindOfClass:[VBMathParserTokenOperation class]]) {
+                actRes = [(VBMathParserTokenOperation*)token evaluateWithArgLeft:paramLeft
+                                                                        argRight:paramRight];
+            }
+            else if ([token isKindOfClass:[VBMathParserTokenFunction class]]) {
+                actRes = [(VBMathParserTokenFunction*)token evaluateWithArg:paramRight];
+            }
+            
+            [resultArray removeObjectsInRange:replacementRange];
+            [resultArray insertObject:@(actRes)
+                              atIndex:replacementRange.location];
+            
+            i = replacementRange.location - 1;
+            
+        }
+    }
+    
+    double result = [resultArray.lastObject doubleValue];
+    return result;
+}
 
+- (void) replaceVarTokens:(NSMutableArray*)tokens
+               withValues:(NSDictionary*)varsValues {
+    [tokens enumerateObjectsUsingBlock:^(VBMathParserToken*  _Nonnull token, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([token isKindOfClass:[VBMathParserTokenVar class]]) {
+            NSNumber* value = varsValues[token.stringValue];
+            if (value && [value isKindOfClass:[NSNumber class]]) {
+                [tokens replaceObjectAtIndex:idx
+                                  withObject:[VBMathParserTokenNumber tokenWithString:value.stringValue]];
+            }
+            else{
+                @throw [VBMathParserMissingValueForVarException exceptionWithVar:token.stringValue];
+            }
+        }
+    }];
+}
+
+- (void) replaceConstTokens:(NSMutableArray*)tokens {
+    [tokens enumerateObjectsUsingBlock:^(VBMathParserToken*  _Nonnull token, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([token isKindOfClass:[VBMathParserTokenConst class]]) {
+            [tokens replaceObjectAtIndex:idx
+                              withObject:[VBMathParserTokenNumber tokenWithString:
+                                          @(((VBMathParserTokenConst*)token).doubleValue).stringValue]];
+        }
+    }];
+}
 
 @end
