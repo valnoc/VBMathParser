e38191e05467d27487e0d68aa36fd70719785ac5
Valeriy Bezuglyy <valnocorner@gmail.com>
2016-03-27 15:19:06 +0300

    pods

---
+++ Example/Pods/Target Support Files/Pods-VBMathParserExamples-VBMathParserExamplesTests/Pods-VBMathParserExamples-VBMathParserExamplesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VBMathParserExamples_VBMathParserExamplesTests : NSObject
+@end
+@implementation PodsDummy_Pods_VBMathParserExamples_VBMathParserExamplesTests
+@end
---
+++ Example/Pods/Target Support Files/Pods-VBMathParserExamples/Pods-VBMathParserExamples-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VBMathParserExamples : NSObject
+@end
+@implementation PodsDummy_Pods_VBMathParserExamples
+@end
